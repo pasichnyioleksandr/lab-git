9ca22b6859d3e3aa53f6cc686b651b8d6b402dca
Oleksandr <pasichnyi.oleksandr1125@vu.cdu.edu.ua>
2025-11-04 10:57:05 +0200

    Перший комміт: добавлені файли проекта LabGit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LabGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вивід вітального повідомлення
+            Console.WriteLine("Привіт! Це моя перша програма для практичної роботи з Git.");
+
+            // Проста взаємодія з користувачем
+            Console.Write("Введіть своє ім'я: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Радий знайомству, " + name + "!");
+
+            // Проста арифметична операція
+            Console.Write("Введіть перше число: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введіть друге число: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int sum = a + b;
+            Console.WriteLine("Сума чисел = " + sum);
+
+            // Завершення програми
+            Console.WriteLine("Дякую за використання програми!");
+        }
+    }
+}
